0ca5563b0aadfbc67b81f8080fe369662f2518e8
Kamil <wienio96@gmail.com>
2020-03-03 15:36:39 +0100

    Update loop.js

--- 02-JSCoreConcepts/loop.js
+++ 02-JSCoreConcepts/loop.js
@@ -1,4 +1,9 @@
-console.log(2);
-console.log(6);
-console.log(10);
-console.log(14);
+let i = 2;
+while(i <= 14) {
+    console.log(i)
+    i+=4
+}
+
+for(i = 2 ; i <=14 ; i+=4) {
+    console.log(i)
+}
