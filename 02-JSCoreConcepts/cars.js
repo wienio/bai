ccf80406f31d951a2dfcab0e1de04f3763d5dbfd
Kamil <wienio96@gmail.com>
2020-03-03 15:33:31 +0100

    Update cars.js

--- 02-JSCoreConcepts/cars.js
+++ 02-JSCoreConcepts/cars.js
@@ -1,5 +1,5 @@
 function isCar(car1, car2, car3) {
-    // return 
+    return (car1 && car2 && car3) || (!car1 && !car2 && !car3);
 }
 
 console.log(isCar(true, true, true));
@@ -7,4 +7,3 @@ console.log(isCar(false, false, false));
 console.log(isCar(true, true, false));
 console.log(isCar(false, false, true));
 console.log(isCar(true, false, true));
-
