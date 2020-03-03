36d380773303b34bd2d3299d3dbba227ad1e9f9b
Kamil <wienio96@gmail.com>
2020-03-03 15:42:54 +0100

    Update address.js

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -1,12 +1,16 @@
-title = "dr";
-name = "Mateusz";
-surname = 'Kubicki';
-street = 'Długa 15';
-city = 'Kraków';
-zip = '30-781';
-country = 'Poland';
+let title = 'dr';
+let name = 'Mateusz';
+let surname = 'Kubicki';
+let street = 'Długa 15';
+let city = 'Kraków';
+let zip = '30-781';
+let country = 'Poland';
 
+let firstOption = title + ' ' + name + ' ' + surname + '\nul. ' + street + '\n' + zip + ' ' + city + '\n' + country.toUpperCase()
+let secondOption = `${title} ${name} ${surname}\nul. ${street}\n${zip} ${city}\n${country.toUpperCase()}`
 
+console.log(firstOption);
+console.log(secondOption);
 /* 
 dr Mateusz Kubicki
 ul. Długa 15
